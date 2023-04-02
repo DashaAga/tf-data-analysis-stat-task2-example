d0e41bd87c63f7f0197adb6c701da0819929168a
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-02 14:17:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import norm
+
+from scipy.stats import t
 
 
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
@@ -9,6 +10,11 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    return x.mean() + 35 * norm.ppf(alpha / 2) / np.sqrt(len(x)), \
-           x.mean() + 35 * norm.ppf(1 - alpha / 2) / np.sqrt(len(x))
+    std = 0
+    alpha = 1-p
+    for i in x:
+      std = std+(i - x.mean())**2
+    std = (std / len(x))**0.5
+    SE = std/(len(x)**0.5)
+    s = (x.mean()-t.ppf(1-alpha/2, len(x)-1)*SE, x.mean()+t.ppf(1-alpha/2, len(x)-1)*SE)
+    return s
